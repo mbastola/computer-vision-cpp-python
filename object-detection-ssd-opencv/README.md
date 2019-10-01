e7eb64d472078b979ba0e161e95a5c9612e61f50
mbastola <mbastola@wustl.edu>
2019-09-30 22:43:39 -0500

    added sample outputs

---
+++ object-detection-ssd-opencv/README.md
@@ -0,0 +1,6 @@
+Object detection example using opencv dnn module. Imports pre-trained caffe/tensforflow models. 
+
+![png](imgs/beach.png)
+![png](imgs/highway.gif)
+![png](imgs/busy_street.gif)
+
--- object-detection-ssd-opencv/ssd.py
+++ object-detection-ssd-opencv/ssd.py
@@ -16,9 +16,6 @@ import json
 import cv2 as cv
 from datetime import datetime
 
-#0 for 20 class Caffe model, 1 for 90 class Tensorflow Model
-MODEL = 0
-
 def readJson(filename):
     with open(filename) as json_file:
         data = json.load(json_file)
@@ -47,77 +44,94 @@ def TfNet():
     Model["conf_thresh"] = 0.2
     return Model
 
+def detect(Model, frame, outfilename):
+    
+    
+    inScaleFactor = 0.007843
+    meanVal = 127.5
+
+    inWidth = 300
+    #inHeight = 300
+    #WHRatio = 1
+
+    w_, h_, _ = frame.shape
+    WHRatio = h_ / float(w_)
+    inHeight = int(inWidth/WHRatio)
+  
+
+    blob = cv.dnn.blobFromImage(frame, inScaleFactor, (inWidth, inHeight), (meanVal, meanVal, meanVal), bool(Model["idx"]))
+    Model["model"].setInput(blob)
+    detections = Model["model"].forward()
+
+    cols = frame.shape[1]
+    rows = frame.shape[0]
+
+    if cols / float(rows) > WHRatio:
+        cropSize = (int(rows * WHRatio), rows)
+    else:
+        cropSize = (cols, int(cols / WHRatio))
+
+    y1 = int((rows - cropSize[1]) / 2)
+    y2 = y1 + cropSize[1]
+    x1 = int((cols - cropSize[0]) / 2)
+    x2 = x1 + cropSize[0]
+    frame = frame[y1:y2, x1:x2]
+    
+    cols = frame.shape[1]
+    rows = frame.shape[0]
+
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > Model["conf_thresh"]:
+            class_id = int(detections[0, 0, i, 1])
+
+            #get bounding boxes
+            x0 = int(detections[0, 0, i, 3] * cols)
+            y0 = int(detections[0, 0, i, 4] * rows)
+            x1   = int(detections[0, 0, i, 5] * cols)
+            y1   = int(detections[0, 0, i, 6] * rows)
+
+            cv.rectangle(frame, (x0, y0), (x1, y1),(0, 255, 0))
+            if str(class_id) in Model["classes"]:
+                label = Model["classes"][str(class_id)] + ": " + str(confidence)
+                labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+                yLeftBottom = max(y0, labelSize[1])
+                cv.rectangle(frame, (x0, y0 - labelSize[1]),(x0 + labelSize[0], y0 + baseLine),(255, 255, 255), cv.FILLED)
+                cv.putText(frame, label, (x0, y0),cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+    #cv.imshow("detections", frame)
+    cv.imwrite(outfilename, frame)    
+
+#0 for 20 class Caffe model, 1 for 90 class Tensorflow Model
+MODEL = 1
+VIDEO = 0
+
 def main():
-    videopath = None
+    path = None
     if len(sys.argv) > 1:
-        videopath = sys.argv[1];
+        path = sys.argv[1];
     Model = None
     if MODEL == 0:
         Model = CaffeNet()
     else:
         Model = TfNet()
 
-    cap = cv.VideoCapture(videopath or 0)
-    
-
-    inWidth = 300
-    inScaleFactor = 0.007843
-    meanVal = 127.5
-
-    
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        width = cap.get(cv.CAP_PROP_FRAME_WIDTH )
-        height = cap.get(cv.CAP_PROP_FRAME_HEIGHT )
-        WHRatio = width / float(height)
-        
-        inHeight = int(inWidth*WHRatio)
-
-        blob = cv.dnn.blobFromImage(frame, inScaleFactor, (inWidth, inHeight), (meanVal, meanVal, meanVal), bool(Model["idx"]))
-        Model["model"].setInput(blob)
-        detections = Model["model"].forward()
-
-        cols = frame.shape[1]
-        rows = frame.shape[0]
-
-        if cols / float(rows) > WHRatio:
-            cropSize = (int(rows * WHRatio), rows)
-        else:
-            cropSize = (cols, int(cols / WHRatio))
-
-        y1 = int((rows - cropSize[1]) / 2)
-        y2 = y1 + cropSize[1]
-        x1 = int((cols - cropSize[0]) / 2)
-        x2 = x1 + cropSize[0]
-        frame = frame[y1:y2, x1:x2]
-
-        cols = frame.shape[1]
-        rows = frame.shape[0]
-
-        for i in range(detections.shape[2]):
-            confidence = detections[0, 0, i, 2]
-            if confidence > Model["conf_thresh"]:
-                class_id = int(detections[0, 0, i, 1])
-
-                #get bounding boxes
-                x0 = int(detections[0, 0, i, 3] * cols)
-                y0 = int(detections[0, 0, i, 4] * rows)
-                x1   = int(detections[0, 0, i, 5] * cols)
-                y1   = int(detections[0, 0, i, 6] * rows)
-
-                cv.rectangle(frame, (x0, y0), (x1, y1),(0, 255, 0))
-                if str(class_id) in Model["classes"]:
-                    label = Model["classes"][str(class_id)] + ": " + str(confidence)
-                    labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-                    yLeftBottom = max(y0, labelSize[1])
-                    cv.rectangle(frame, (x0, y0 - labelSize[1]),(x0 + labelSize[0], y0 + baseLine),(255, 255, 255), cv.FILLED)
-                    cv.putText(frame, label, (x0, y0),cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-
-        #cv.imshow("detections", frame)
-        cv.imwrite("output/detections{}.png".format(uniqueStr()), frame)
-        #if cv.waitKey(1) >= 0:
+    if VIDEO:
+        cap = cv.VideoCapture(path or 0)
+        j = 0
+        while True:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            output = "output/detections{}.png".format(uniqueStr())
+            #output = "output/detections_{}.png".format(j)
+            detect(Model, frame, output)
+            j+=1
+            #if cv.waitKey(1) >= 0:
             #break
+    else:
+        frame = cv.imread(path, cv.IMREAD_COLOR)
+        output = "output/detections{}.png".format(uniqueStr())
+        detect(Model,frame, output)
 
 if __name__ == "__main__":
     main()
